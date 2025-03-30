20837865947b1b8bdd2ceb9a95fc8a1fdd88eb44
Leandro <leandromllr9@gmail.com>
2025-03-30 19:12:02 -0300

    refactor: ajusta compoente input para receber icon no canto direito

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,34 +6,48 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: ReactNode;
   error?: string;
   className?: string;
+  rightIcon?: ReactNode;
 }
 
-export function Input({ label, icon, error, className, ...props }: InputProps) {
+export function Input({
+  label,
+  icon,
+  error,
+  className,
+  rightIcon,
+  ...props
+}: InputProps) {
   return (
     <div className="flex flex-col gap-1">
       {label && (
-        <label htmlFor={props.id} className="text-text-secondary text-sm">
+        <label htmlFor={props.id} className="text-sm text-text-secondary">
           {label}
         </label>
       )}
 
       <div className="relative">
         {icon && (
-          <span className="text-text-secondary absolute left-3 top-1/2 -translate-y-1/2">
+          <span className="absolute left-3 top-1/2 -translate-y-1/2 text-text-secondary">
             {icon}
           </span>
         )}
         <input
           {...props}
           className={twMerge(
-            `text-md text-text-primary placeholder:text-text-secondary w-full rounded-md border border-border bg-white px-4 py-2 shadow-sm outline-none transition focus:border-primary focus:ring-1 focus:ring-primary`,
+            `text-md w-full rounded-md border border-border bg-white px-4 py-2 text-text-primary shadow-sm outline-none transition placeholder:text-text-secondary focus:border-primary focus:ring-1 focus:ring-primary`,
             icon ? "pl-10" : "",
+            rightIcon ? "pr-10" : "",
             error
               ? "border-red-500 focus:border-red-500 focus:ring-red-500"
               : "",
             className,
           )}
         />
+        {rightIcon && (
+          <span className="absolute right-3 top-1/2 -translate-y-1/2 text-text-secondary">
+            {rightIcon}
+          </span>
+        )}
       </div>
 
       {error && <p className="text-sm text-red-500">{error}</p>}
