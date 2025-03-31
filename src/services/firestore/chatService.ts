7add8e4af8204a87c3185afbfe1b2e6870f03e30
Leandro <leandromllr9@gmail.com>
2025-03-30 21:00:04 -0300

    feat: cria funções para criar um chat e listar todos os chats de um usuário

--- src/services/firestore/chatService.ts
+++ src/services/firestore/chatService.ts
@@ -1,22 +1,54 @@
 import { firestore } from "@/lib/firebase"
-import { doc, getDoc, setDoc } from "firebase/firestore"
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, Timestamp, where } from "firebase/firestore"
 import { chatConverter } from "../firebase/converter/chatConverter"
 import { ChatProps } from "@/types/Chat"
+import { AppUserProps } from "@/types/User"
 
 const chatRef = (chatId: string) => {
   return doc(firestore, "chats", chatId).withConverter(chatConverter)
 }
 
+const chatCollectionConverted = collection(firestore, "chats").withConverter(chatConverter)
+
 export const createChat = async (chat: ChatProps): Promise<ChatProps> => {
   const ref = chatRef(chat.id)
   const existing = await getDoc(ref)
 
-  if(existing) {
-    console.log("Já existe chat entre os usuários");
+  if(existing.data()) {
     return existing.data() as ChatProps;
   }
 
   await setDoc(ref, chat);
-
   return chat;
+}
+
+export const createChatWithUser = async (appUser: AppUserProps, selectedUser: AppUserProps): Promise<void> => {
+  const chatId = [appUser.uid, selectedUser.uid].sort().join("_");
+
+  const chat: ChatProps = {
+    id: chatId,
+    users: [appUser.uid, selectedUser.uid],
+    typing: {
+      [appUser.uid]: false,
+      [selectedUser.uid]: false,
+    },
+    unreadCountByUser: {
+      [appUser.uid]: 0,
+      [selectedUser.uid]: 0,
+    },
+    lastMessage: "",
+    lastMessageTime: null,
+    lastMessageSender: "",
+    createdAt: serverTimestamp() as Timestamp,
+    updatedAt: serverTimestamp() as Timestamp
+  }
+
+  await createChat(chat)
+}
+
+export const getAllChatsUser = async (userUid: string) => {
+  const q = query(chatCollectionConverted, where("users", "array-contains", userUid))
+  const snapshot = await getDocs(q)
+
+  return snapshot.docs.map((doc) => doc.data())
 }
