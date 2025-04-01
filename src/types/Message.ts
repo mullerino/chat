be548bd1ae6010491ded0131a1f348acd737734a
Leandro <leandromllr9@gmail.com>
2025-03-31 22:52:03 -0300

    refactor: cria mais types para message

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,6 +1,22 @@
+import { Timestamp } from "firebase/firestore";
+import { AppUserProps } from "./User";
+
 export interface MessageProps {
   id?: string;
   text: string;
   time: string;
   isOwnMessage?: boolean;
+}
+export interface MessageFirebaseProps {
+  id: string;
+  text: string;
+  time: Timestamp;
+  senderId: string;
+  status: string;
+}
+
+export interface SendMessageProps {
+  chatId: string,
+  message: string,
+  user: AppUserProps
 }
