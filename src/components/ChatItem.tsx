1c84d5108d3a2a926b774bc48c6de35bd9745607
Leandro <leandromllr9@gmail.com>
2025-04-01 23:02:46 -0300

    feat: implementa a alteração visual de um chat ao clicar em um chatItem

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -2,27 +2,32 @@ import { formatTime } from "@/utils/timestampConverter";
 import { Timestamp } from "firebase/firestore";
 
 interface ChatItemProps {
-  name: string;
+  name: string | undefined;
   timestamp: Timestamp | null;
   lastMessage: string;
   unread?: number;
+  onClick: () => void;
 }
 
 export default function ChatItem({
   name,
   timestamp,
   lastMessage,
+  onClick,
 }: ChatItemProps) {
   return (
-    <div className="flex cursor-pointer items-center gap-3 rounded-md px-4 py-3 text-text-secondary transition hover:bg-background">
+    <div
+      className="flex cursor-pointer items-center gap-3 rounded-md px-4 py-3 text-text-secondary transition hover:bg-background"
+      onClick={onClick}
+    >
       <img
-        src="https://ui-avatars.com/api/?name=Leandro&background=5D5FEF&color=fff"
+        src={`https://ui-avatars.com/api/?name=${name}&background=5D5FEF&color=fff`}
         className="h-12 w-12 rounded-full object-cover"
       />
       <div className="flex min-w-0 flex-1 flex-col gap-1">
         <div className="flex items-center justify-between">
           <h3 className="truncate text-sm font-medium text-text-primary">
-            leo
+            {name}
           </h3>
           <span className="text-right text-xs">{formatTime(timestamp)}</span>
         </div>
