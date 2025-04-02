b324ef9f35713162fbe7dfc994ce3e41cc30d214
Leandro <leandromllr9@gmail.com>
2025-04-01 23:04:07 -0300

    feat: implementa useEffect em conjunto com o onSnapshot para monitoramento das collections em tempo real

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -10,43 +10,79 @@ import {
 import { useAuth } from "./AuthContext";
 import {
   createChatWithUser,
-  getAllChatsUser,
+  listenChatsUser,
 } from "@/services/firestore/chatService";
-import { sendMessage } from "@/services/firestore/messageService";
-import { SendMessageProps } from "@/types/Message";
+import {
+  listenMessages,
+  sendMessage,
+} from "@/services/firestore/messageService";
+import { MessageFirebaseProps } from "@/types/Message";
+import { getAppUserDoc } from "@/services/firestore/userService";
 
 interface ChatContextProps {
   chats: ChatProps[];
-  createChat: (targetUser: AppUserProps) => Promise<void>;
+  otherUser: AppUserProps | null;
+  changeOtherUser: (user: AppUserProps) => void;
+  messages: MessageFirebaseProps[];
   selectedChat: ChatProps | null;
+  isTyping: boolean;
+  unreadCount: { [chatId: string]: number } | undefined;
+  createChat: (targetUser: AppUserProps) => Promise<void>;
   selectChat: (chat: ChatProps) => void;
   sendMessage: (message: string) => void;
-  isTyping: boolean;
   setTyping: (status: boolean) => void;
-  unreadCount: { [chatId: string]: number } | undefined;
 }
 
 const ChatContext = createContext<ChatContextProps | null>(null);
 
 export const ChatProvider = ({ children }: { children: ReactNode }) => {
   const { appUser } = useAuth();
+
   const [chats, setChats] = useState<ChatProps[]>([]);
+  const [messages, setMessages] = useState<MessageFirebaseProps[]>([]);
   const [selectedChat, setSelectedChat] = useState<ChatProps | null>(null);
-  const [isTyping, setisTyping] = useState<boolean>(false);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const [otherUser, setOtherUser] = useState<AppUserProps | null>(null);
   const [unreadCount, setUnreadCount] = useState<{
     [chatId: string]: number;
   }>();
 
   useEffect(() => {
-    const getChats = async () => {
-      if (appUser?.uid) {
-        const chats = await getAllChatsUser(appUser?.uid);
-        setChats(chats);
-      }
-    };
+    if (selectedChat) {
+      const getOtherUser = async () => {
+        const userId = selectedChat.users.find((user) => user !== appUser?.uid);
+        if (userId) {
+          const user = await getAppUserDoc(userId);
+          setOtherUser(user);
+        }
+      };
+      getOtherUser();
+    }
+  }, [selectedChat]);
 
-    getChats();
-  }, [appUser?.uid]);
+  useEffect(() => {
+    if (!appUser?.uid) return;
+
+    const unsubscribe = listenChatsUser(appUser.uid, (chatsUser) => {
+      setChats(chatsUser);
+
+      if (chatsUser.length !== 0 && !selectedChat) {
+        setSelectedChat(chatsUser[0]);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [selectedChat, appUser?.uid]);
+
+  useEffect(() => {
+    if (!selectedChat?.id) return;
+
+    const unsubscribe = listenMessages(selectedChat.id, (msgs) => {
+      setMessages(msgs);
+    });
+
+    return () => unsubscribe();
+  }, [selectedChat?.id]);
 
   const createChat = async (targetUser: AppUserProps): Promise<void> => {
     if (!appUser) throw new Error("Usuário não autenticado");
@@ -61,11 +97,25 @@ export const ChatProvider = ({ children }: { children: ReactNode }) => {
     if (!selectedChat?.id || !appUser)
       throw new Error("Usuário ou chat não encontrado!");
 
-    await sendMessage({ message, chatId: selectedChat?.id, user: appUser });
+    await sendMessage({
+      message,
+      chatId: selectedChat?.id,
+      user: appUser,
+    });
+  };
+
+  const changeOtherUserById = async (userId: string) => {
+    const user = await getAppUserDoc(userId);
+    setOtherUser(user);
   };
 
   const selectChat = (chatSelected: ChatProps) => {
     setSelectedChat(chatSelected);
+    changeOtherUserById(chatSelected.users[1]);
+  };
+
+  const changeOtherUser = (user: AppUserProps) => {
+    setOtherUser(user);
   };
 
   const setTyping = () => {};
@@ -74,10 +124,13 @@ export const ChatProvider = ({ children }: { children: ReactNode }) => {
     <ChatContext.Provider
       value={{
         chats,
+        otherUser,
+        messages,
         selectedChat,
-        createChat,
         isTyping,
         unreadCount,
+        changeOtherUser,
+        createChat,
         selectChat,
         sendMessage: sendMessageUser,
         setTyping,
