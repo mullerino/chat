ed0754e14b1e39da37b436e4f03ebb832af904a4
Leandro <leandromllr9@gmail.com>
2025-04-03 23:07:55 -0300

    refactor: corrige tipagem do erro

--- src/services/firestore/authService.ts
+++ src/services/firestore/authService.ts
@@ -1,11 +1,16 @@
 import { auth } from "@/lib/firebase";
-import { LoginProps } from "@/types/User";
+import { AppUserProps, LoginProps } from "@/types/User";
 import { translateFirebaseError } from "@/utils/translateFirebaseError";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from "firebase/auth";
 import { toast } from "sonner";
 import { getAppUserDoc } from "./userService";
 
-export const registerUser  = async (email: string, password: string): Promise<User> => {
+interface ILoginProps {
+  appUser: AppUserProps,
+  firebaseUser: User
+}
+
+export const registerUser  = async (email: string, password: string): Promise<User | undefined> => {
   try {
     const createdUser = await createUserWithEmailAndPassword(
       auth,
@@ -14,14 +19,18 @@ export const registerUser  = async (email: string, password: string): Promise<Us
     );
 
     return createdUser.user
-  } catch (err: any) {
-    const message = translateFirebaseError(err.code || err.message);
-    toast.error(message);
-    throw err;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      const message = translateFirebaseError(err.message);
+      toast.error(message);
+      throw err;
+    }
+
+    toast.error("Ocorreu um erro desconhecido.");
   }
 };
 
-export const loginUser = async ({ email, password }: LoginProps) => {
+export const loginUser = async ({ email, password }: LoginProps): Promise<ILoginProps> => {
   try {
     const result = await signInWithEmailAndPassword(auth, email, password);
     const firebaseUser: User = result.user;
@@ -29,10 +38,15 @@ export const loginUser = async ({ email, password }: LoginProps) => {
     const appUser = await getAppUserDoc(firebaseUser.uid)
 
     return { appUser, firebaseUser }
-  } catch (err: any) {
-    const message = translateFirebaseError(err.code || err.message);
-    toast.error(message);
-    throw err;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      const message = translateFirebaseError(err.message);
+      toast.error(message);
+      throw err;
+    }
+
+    toast.error("Ocorreu um erro desconhecido.");
+    throw new Error("Erro desconhecido");
   }
 };
 
