0c45bdd0424e1a75f8283299a32915e657f9f136
Leandro <leandromllr9@gmail.com>
2025-04-03 23:01:11 -0300

    feat: implementa contagem das mensagens não lidas na interface

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -6,6 +6,7 @@ interface ChatItemProps {
   timestamp: Timestamp | null;
   lastMessage: string;
   unread?: number;
+  isActive: boolean;
   onClick: () => void;
 }
 
@@ -13,16 +14,23 @@ export default function ChatItem({
   name,
   timestamp,
   lastMessage,
+  unread,
+  isActive,
   onClick,
 }: ChatItemProps) {
   return (
     <div
-      className="flex cursor-pointer items-center gap-3 rounded-md px-4 py-3 text-text-secondary transition hover:bg-background"
+      className={`flex cursor-pointer items-center gap-3 rounded-md px-4 py-3 text-text-secondary transition-all duration-200 ${
+        isActive
+          ? "border-l-4 border-violet-500 bg-indigo-50 shadow-sm"
+          : "bg-white hover:bg-gray-100"
+      }`}
       onClick={onClick}
     >
       <img
         src={`https://ui-avatars.com/api/?name=${name}&background=5D5FEF&color=fff`}
         className="h-12 w-12 rounded-full object-cover"
+        alt={`${name}`}
       />
       <div className="flex min-w-0 flex-1 flex-col gap-1">
         <div className="flex items-center justify-between">
@@ -33,9 +41,11 @@ export default function ChatItem({
         </div>
         <div className="flex items-center justify-between">
           <span className="truncate text-sm">{lastMessage}</span>
-          <span className="ml-1 rounded-full bg-primary/10 px-2 py-0.5 text-center text-xs font-bold text-primary">
-            1
-          </span>
+          {unread !== 0 && (
+            <span className="ml-1 rounded-full bg-primary/10 px-2 py-0.5 text-center text-xs font-bold text-primary">
+              {unread}
+            </span>
+          )}
         </div>
       </div>
     </div>
