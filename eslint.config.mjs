e5b6d102e3f2cb3829f972916cac9778de7ce4cf
Leandro <leandromllr9@gmail.com>
2025-04-03 22:58:57 -0300

    chore: altera regra do eslint para permitir uso da tag img

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off", // Desativa o aviso para <img />
+    },
+  },
 ];
 
 export default eslintConfig;
